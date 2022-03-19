feddb2349f3cc299feef206b70e4600530a3e23d
aghil p wilson <aghilpwilson1994@gmail.com>
2022-03-19 18:56:09 +0530

    music player

---
+++ src/components/Library.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import LibrarySong from './LibrarySong'
+const Library = ({libraryStatus,songs,setCurrentSong,id,audioRef,isPlaying,setSongs}) => {
+  return (
+    <div className={`library ${libraryStatus ? 'active-library':''}`}>
+        <h2>Library</h2>
+        <div className="library-songs">
+        {songs.map(song=>(
+            <LibrarySong setCurrentSong={setCurrentSong}  song={song}  songs={songs} id={song.id} key={song.id} audioRef={audioRef} isPlaying={isPlaying} setSongs={setSongs}/>
+        ))}
+       
+
+        </div>
+    </div>
+  )
+}
+
+export default Library;
---
+++ src/data.js
@@ -0,0 +1,118 @@
+import {v4 as uuidv4} from "uuid";
+
+function chillHop(){
+    return [
+        {
+            name:"Vision Nocturne",
+            artist:"C Y G N",
+            cover:"https://chillhop.com/wp-content/uploads/2022/03/2ac741e8e52e0008d1fb9cbfcdacd2cf9eaef2db-300x300.jpg",
+            id:uuidv4(),
+            color:["#2D4F66","#545D69"],
+            audio:"https://mp3.chillhop.com/serve.php/?mp3=31608",
+            active:true,
+        },
+        {
+            name:"Drift",
+            artist:"Somini",
+            cover:"https://chillhop.com/wp-content/uploads/2022/01/d7c0bd347f56540babd9dd85454b93a89df84a15-150x150.jpg",
+            id:uuidv4(),
+            color:["#996959","#1C3433"],
+            audio:"https://mp3.chillhop.com/serve.php/?mp3=30133",
+            active:false
+        },
+        {
+            name:"In My Head",
+            artist:"Misha, NDO, Flozigg",
+            cover:"https://chillhop.com/wp-content/uploads/2022/01/6881c7456483ab8ea364152a594942a20669a058-150x150.jpg",
+            id:uuidv4(),
+            color:["#2C3866","#D8CEB4"],
+            audio:"https://mp3.chillhop.com/serve.php/?mp3=28878",
+            active:false
+        },
+        {
+            name:"Santiago",
+            artist:"Psalm Trees, Moods",
+            cover:"https://chillhop.com/wp-content/uploads/2022/01/1abf44fbb1364ca8435a8bbf4c5750a80947128f-300x300.jpg",
+            id:uuidv4(),
+            color:["#FEF0D4","#9C9FA2"],
+            audio:"https://mp3.chillhop.com/serve.php/?mp3=30202",
+            active:false
+        },
+        {
+            name:"Seagulls",
+            artist:"Misha, Zmeyev, Viktor Minsky",
+            cover:"https://i.pinimg.com/originals/9a/d3/3e/9ad33e90caeebd64bf7cee8601310c77.jpg",
+            id:uuidv4(),
+            color:["#EEF01A","#A4128C"],
+            audio:"https://mp3.chillhop.com/serve.php/?mp3=28936",
+            active:false
+        },
+        {
+            name:"Sunday brunch",
+            artist:"Nymano, Zeyn, Bënji",
+            cover:"https://www.billboard.com/wp-content/uploads/media/Reefer-Revolution-Trippiest-Album-Covers-a-Silo-2017-billboard-1548.jpg",
+            id:uuidv4(),
+            color:["#728303","#A4128C"],
+            audio:"https://mp3.chillhop.com/serve.php/?mp3=28977",
+            active:false
+        },
+        {
+            name:"Condition Zero",
+            artist:"HM Surf",
+            cover:"https://cdn.domestika.org/c_fit,dpr_auto,f_auto,t_base_params,w_820/v1589500982/content-items/004/567/801/13th_Floor_Elevators_Psychedelic-original.jpg?1589500982",
+            id:uuidv4(),
+            color:["#A5BE2D","#192123"],
+            audio:"https://mp3.chillhop.com/serve.php/?mp3=30094",
+            active:false
+        },
+        {
+            name:"Lake Placido",
+            artist:"The Doppelgangaz",
+            cover:"https://www.chicagotribune.com/resizer/aBk7JGuJgP30MSTpxrtgutN2FgY=/800x800/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/4SWPZ3U5TRGL3JICDUWS5XTJQU.jpg",
+            id:uuidv4(),
+            color:["#040404","#171717"],
+            audio:"https://mp3.chillhop.com/serve.php/?mp3=30337",
+            active:false
+        },
+        {
+            name:"Embers",
+            artist:"Molly McPhaul, Enluv",
+            cover:"https://cdn.domestika.org/c_fit,dpr_auto,f_auto,t_base_params,w_820/v1589501538/content-items/004/567/918/image2-original.jpeg?1589501538",
+            id:uuidv4(),
+            color:["#E35A89","#D46E46"],
+            audio:"https://mp3.chillhop.com/serve.php/?mp3=30135",
+            active:false
+        },
+        {
+            name:"Reunion",
+            artist:"Stan Forebee",
+            cover:"https://d1csarkz8obe9u.cloudfront.net/posterpreviews/song-cover-video-trippy-design-template-3e4c7093b5ce379572b48f253fb8e581_screen.jpg?ts=1591430052",
+            id:uuidv4(),
+            color:["#F38191","#195368"],
+            audio:"https://mp3.chillhop.com/serve.php/?mp3=30337",
+            active:false
+        },
+        {
+            name:"Waybackwhen",
+            artist:"Philanthrope, Parkbench Epiphany",
+            cover:"https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/e33c6e45362059.582ddfb485a00.png",
+            id:uuidv4(),
+            color:["#E95F3E","#A4989C"],
+            audio:"https://mp3.chillhop.com/serve.php/?mp3=27502",
+            active:false
+        },
+        {
+            name:"Colorblind",
+            artist:"Shrimpnose",
+            cover:"https://c4.wallpaperflare.com/wallpaper/628/108/928/sunman24-cover-art-psychedelic-music-wallpaper-thumb.jpg",
+            id:uuidv4(),
+            color:["#728303","#A4128C"],
+            audio:"https://mp3.chillhop.com/serve.php/?mp3=28977",
+            active:false
+        },
+    ]
+}
+
+
+
+export default chillHop;
